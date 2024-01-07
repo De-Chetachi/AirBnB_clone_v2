b99a3d523841539351828e704d0bf5eec1ac5d15
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-01-07 15:20:51 +0100

    me, so i 4got to use the abs path of command

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -3,55 +3,42 @@
 exec { 'update':
   command => '/usr/bin/apt-get update',
 }
-package { 'nginx':
+
 -> package { 'nginx':
   require => Exec['update'],
   ensure => installed,
 }
 
-exec { '/data':
 -> exec { '/data':
   command => '/usr/bin/mkdir -p "/data/web_static/shared/"',
 }
 
-exec { '/data/web_static/releases/test':
 -> exec { '/data/web_static/releases/test':
   command => '/usr/bin/mkdir -p "/data/web_static/releases/test/"',
 }
 
-exec { 'html_content': 
 -> exec { 'html_content': 
   command => '/usr/bin/echo "test nginx config" > "/data/web_static/releases/test/index.html"',
 }
 
-exec { 'symlink':
 -> exec { 'symlink':
   command => '/usr/bin/ln -sf "/data/web_static/releases/test/" "/data/web_static/current"',
 }
 
-exec {'ownership':
 -> exec {'ownership':
   command => '/usr/bin/chown -R ubuntu:ubuntu /data/',
 }
 
-exec {'restart':
 -> exec {'restart':
   command => '/usr/sbin/service nginx restart',
 }
 
-file { 'hbnb_static':
 -> file { 'hbnb_static':
   path => '/etc/nginx/sites-available/default',
   ensure => 'present',
   content => template('nginx_config'),
 }
 
-exec {'restart':
 -> exec {'restart':
   command => '/usr/sbin/service nginx restart',
-}
-
-service { 'nginx':
-  ensure => running,
-  require => Package['nginx'],
 }
