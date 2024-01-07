77f8ba4d99635e5b36ca968d062c00a0af8b7f0c
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-01-07 15:19:36 +0100

    me, so i 4got to use the abs path of command

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,29 +1,57 @@
-# instal and configure nginx
-exec {'update':
+#web static setup
+
+exec { 'update':
   command => '/usr/bin/apt-get update',
 }
+package { 'nginx':
 -> package { 'nginx':
+  require => Exec['update'],
   ensure => installed,
 }
--> exec { 'run1':
-  command => '/usr/bin/mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"',
+
+exec { '/data':
+-> exec { '/data':
+  command => '/usr/bin/mkdir -p "/data/web_static/shared/"',
 }
--> exec { 'run2':
-  command => '/usr/bin/echo "Hi!" | sudo tee /data/web_static/releases/test/index.html > /dev/null',
+
+exec { '/data/web_static/releases/test':
+-> exec { '/data/web_static/releases/test':
+  command => '/usr/bin/mkdir -p "/data/web_static/releases/test/"',
 }
--> exec { 'run3':
-  command => '/usr/bin/rm -rf /data/web_static/current',
+
+exec { 'html_content': 
+-> exec { 'html_content': 
+  command => '/usr/bin/echo "test nginx config" > "/data/web_static/releases/test/index.html"',
 }
--> exec { 'run4':
-  command => '/usr/bin/ln -s /data/web_static/releases/test/ /data/web_static/current',
+
+exec { 'symlink':
+-> exec { 'symlink':
+  command => '/usr/bin/ln -sf "/data/web_static/releases/test/" "/data/web_static/current"',
 }
--> exec { 'run5':
+
+exec {'ownership':
+-> exec {'ownership':
   command => '/usr/bin/chown -R ubuntu:ubuntu /data/',
 }
--> exec { 'hbnb_static':
-  command => 'sudo sed -i "/^server {/a \ \n\tlocation \/hbnb_static {alias /data/web_static/current/;index index.html;}" /etc/nginx/sites-enabled/default',
-  provider => shell,
-}
--> exec { 'run6':
+
+exec {'restart':
+-> exec {'restart':
   command => '/usr/sbin/service nginx restart',
+}
+
+file { 'hbnb_static':
+-> file { 'hbnb_static':
+  path => '/etc/nginx/sites-available/default',
+  ensure => 'present',
+  content => template('nginx_config'),
+}
+
+exec {'restart':
+-> exec {'restart':
+  command => '/usr/sbin/service nginx restart',
+}
+
+service { 'nginx':
+  ensure => running,
+  require => Package['nginx'],
 }
