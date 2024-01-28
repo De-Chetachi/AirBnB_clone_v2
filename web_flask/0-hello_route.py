205a14f1a34cecaf8d47b21bdd6a5d0cb630fc43
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-01-28 16:42:36 +0100

    pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 '''this module is a script that starts a flask web application'''
-#import the Flask class 
+# import the Flask class 
 from flask import Flask
 
-#create an instance of the class
+# create an instance of the class
 app = Flask(__name__)
 
-#use the route decorator to tell flask which url(path) should trigger the root function
+# use the route decorator to tell flask which url(path) should use the root fxn
 @app.route("/", strict_slashes=False)
 def root():
     '''handles the app logic for the root query (/)'''
