037e1008dd20b28fb12d90bbf5dd294f8bb61c32
Onyenze Chetachi <chetaGod4life@gmail.com>
2024-01-28 22:13:52 +0100

    try

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''this module is a script that starts a flask web application'''
+from models import storage
+from models.state import State
+# import the Flask class
+from flask import Flask
+from flask import  render_template
+# create an instance of the class
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    '''close session after each request to avoid complication with sess mnt'''
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list_route():
+    '''handles the app logic for the root query (/states_list)'''
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
